4e73f7ca2158da2d581f302745a36f3be2c49834
Igor Schöenardie Viegas <igorsviegas26@gmail.com>
2025-12-05 23:46:04 -0300

    feat: inclui a lib do shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 
 import App from "./App.tsx";
+import "./styles/tailwindcss.css";
 
 
 createRoot(document.getElementById('root')!).render(
