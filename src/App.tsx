ebc3fc2f466e7751039a13b0d3c4d80061e09a22
Igor Schöenardie Viegas <igorsviegas26@gmail.com>
2025-12-07 12:03:53 -0300

    feat: inclui a opção de retorno ao topo da página

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { academic } from "./data/academic";
+import { Button } from "./components/ui/button";
 import { professional } from "./data/professional";
 import { Profile } from "./components/custom/Profile";
 import { profile } from "./data/profile";
+import { RiArrowUpSLine } from "react-icons/ri";
 import { Skills } from "./components/custom/Skills";
 import { skills } from "./data/skills";
 import { Timeline } from "./components/custom/Timeline";
@@ -9,6 +11,13 @@ import { Footer } from "./components/custom/Footer";
 
 
 function App() {
+  const handleUp = () => {
+    window.scrollTo({
+      behavior: "smooth",
+      top     : 0
+    });
+  };
+  
   return (
     <div className="min-h-screen select-none text-white w-full">
       <div className="h-screen mx-auto w-10/12 md:w-8/12 lg:w-9/12 xl:w-7/12">
@@ -17,6 +26,9 @@ function App() {
         <Timeline data={ academic } title="Linha do tempo acadêmica" />
         <Skills data={ skills } title="Habilidades/conhecimentos" />
         <Footer />
+        <Button className="bottom-5 cursor-pointer fixed right-5 z-50" onClick={ handleUp } size="sm" variant="secondary">
+          <RiArrowUpSLine />
+        </Button>
       </div>
     </div>
   );
